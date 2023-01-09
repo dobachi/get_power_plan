ae7a53c81e56a49142d1d13e1e8ed7f8f4468acf
dobachi <dobachi1983oss@gmail.com>
2023-01-10 00:00:49 +0900

    initial commit

---
+++ get_power_mode.ps1
@@ -0,0 +1,26 @@
+﻿using namespace System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+[Application]::EnableVisualStyles()
+
+# フォーム
+$frame = New-Object Form -Property @{
+    Text            = '電源プランの確認'
+    Size            = New-Object Drawing.Size(300, 200)
+    MaximizeBox     = $false
+    FormBorderStyle = 'FixedDialog'
+    Font            = New-Object Drawing.Font('Meiryo UI', 30)
+}
+
+# ラベル
+$Label1 = New-Object Label -Property @{
+    Text        = ((powercfg.exe /GETACTIVESCHEME) -split " \(" -split "\)")[1]
+    #Text        = 'hoge'
+    #Location    = New-Object Drawing.Point(20, 20)
+    AutoSize    = $True
+}
+
+$frame.Controls.AddRange(@($Label1))
+
+#フォームの表示
+$frame.ShowDialog()
